e11eac7ccd63a97c298dbc0e1c381109c4d661db
Laura Frasca <35461777+gvenour@users.noreply.github.com>
2018-02-08 19:34:24 -0800

    Add unit tests for Mission class.

---
+++ test_mission.py
@@ -0,0 +1,46 @@
+import unittest
+import logging
+
+from actor import Avatar
+from mission import Mission
+
+class CombatBalanceTestCase(unittest.TestCase):
+    """Tests for combat win and loss balance."""
+
+    def test_avatar_l1_enemy_l1(self):
+        """Test level 1 avatar vs level 1 enemy."""
+        losses = 0
+        runs = 10000
+        avatar = Avatar()
+        mission = Mission()
+
+        for i in range(runs):
+            avatar.reset_hp()
+            mission.__init__()
+            if not mission.resolve_combat(avatar):
+                losses += 1
+
+        loss_ratio = losses / runs
+        self.assertLessEqual(
+                loss_ratio, 0.15, msg="Losses higher than 15%")
+        self.assertGreaterEqual(
+                loss_ratio, 0.1, msg="Losses lower than 10%")
+
+
+class MissionTestCase(unittest.TestCase):
+    """Tests for combat results."""
+
+    def test_avatar_wins(self):
+        """Test avatar winning."""
+        avatar = Avatar()
+        mission = Mission()
+        mission.enemy.hp = 1
+        self.assertTrue(mission.resolve_combat(avatar))
+
+    def test_avatar_loses(self):
+        """Test avatar losing."""
+        avatar = Avatar()
+        mission = Mission()
+        avatar.hp = 1
+        self.assertFalse(mission.resolve_combat(avatar))
+
